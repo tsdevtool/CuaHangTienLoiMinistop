61d3831e1c4693d2f674c2e053c18c4d68e7c5bf
Luân Lê <luanml0016@gmail.com>
2024-06-21 01:41:51 +0700

    add layoutadmin

---
+++ Melistop/src/main/java/com/example/Melistop/controllers/AdminController.java
@@ -0,0 +1,20 @@
+package com.example.Melistop.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class AdminController {
+
+    @GetMapping("/mainAdmin")
+    public String admin() {
+        return "admin/main";
+    }
+
+    @GetMapping("/indexAdmin")
+    public String indexAdmin() {
+        return "admin/index";
+    }
+
+
+
+}
