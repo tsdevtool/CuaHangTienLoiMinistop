9784982e2e160256fda5e855b73acb5f92d44557
KhanhThoii <luuquangkhanhit02@gmail.com>
2024-06-30 12:48:25 +0700

    lich su don hang

--- Melistop/src/main/java/com/example/Melistop/repository/OrderRepository.java
+++ Melistop/src/main/java/com/example/Melistop/repository/OrderRepository.java
@@ -2,9 +2,13 @@ package com.example.Melistop.repository;
 
 
 import com.example.Melistop.models.Order;
+import com.example.Melistop.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUser(User user);
 }
--- Melistop/src/main/java/com/example/Melistop/service/OrderService.java
+++ Melistop/src/main/java/com/example/Melistop/service/OrderService.java
@@ -1,12 +1,19 @@
 package com.example.Melistop.service;
 
-import lombok.RequiredArgsConstructor;
+import com.example.Melistop.models.Order;
+import com.example.Melistop.models.User;
+import com.example.Melistop.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
-@RequiredArgsConstructor
-@Transactional
 public class OrderService {
+    @Autowired
+    private OrderRepository orderRepository;
 
+    public List<Order> getOrdersByUser(User user) {
+        return orderRepository.findByUser(user);
+    }
 }
