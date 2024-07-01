58d9487de2e4ca6622490b4e84107e5b678c2014
ThanhSieu2k3 <122494878+tsdevtool@users.noreply.github.com>
2024-07-01 23:44:28 +0700

    Done show cart

--- Melistop/src/main/java/com/example/Melistop/repository/CartItemRepository.java
+++ Melistop/src/main/java/com/example/Melistop/repository/CartItemRepository.java
@@ -6,7 +6,9 @@ import com.example.Melistop.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.*;
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     CartItem findByUserAndProduct(User user, Product product);
+    List<CartItem> findByUser(User user);
 }
